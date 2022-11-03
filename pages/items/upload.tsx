279eb8ee3bbd240f13edcf427e98d0bea3510e35
picapipicca <haewon09106@gmail.com>
2022-11-03 17:06:32 +0900

    new:item create page ui

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
     return (
-        <div className={"px-4 py-16"}>
+        <div className={"px-4 space-y-5 py-10"}>
             <div>
                 <label className={"cursor-pointer w-full flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md mb-3 text-gray-600 hover:text-orange-500 hover:border-orange-500"}>
                     <svg
@@ -22,8 +22,17 @@ const Upload: NextPage = () => {
                     <input type="file" className={"hidden"}/>
                 </label>
             </div>
-
-            <div className={"my-5"}>
+            <div >
+                <label  className="mb-1 block text-sm font-medium text-gray-700"
+                        htmlFor="name">상품명</label>
+                <div className="rounded-md relative flex  items-center shadow-sm">
+                    <input  id="name"
+                            type="email"
+                            className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+                            required/>
+                </div>
+            </div>
+            <div>
                 <label className={"text-sm font-medium text-gray-700 mb-1 block"} htmlFor={"price"}>가격</label>
                 <div className="flex items-center rounded-md shadow-sm relative">
                     {/*<div className={"absolute left-0 pl-3 flex items-center justify-center"}>*/}
@@ -36,10 +45,10 @@ const Upload: NextPage = () => {
                 </div>
             </div>
             <div>
-                <label className={"text-sm font-medium text-gray-700 mb-1 block"}>Description</label>
-                    <textarea rows={4} className={"mt-1 shadow-sm w-full focus:ring-emerald-700 rounded-md border-gray-300 focus:border-emerald-800"}/>
+                <label htmlFor="description" className={"text-sm font-medium text-gray-700 mb-1 block"}>설명</label>
+                    <textarea id="description" rows={4} className={"mt-1 shadow-sm w-full focus:ring-emerald-700 rounded-md border-gray-300 focus:border-emerald-800"}/>
             </div>
-            <button className={"bg-emerald-500 hover:bg-emerald-600 text-white py-2 px-4 border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 focus:outline-none mt-6 w-full"}>업로드</button>
+            <button className={"bg-emerald-500 hover:bg-emerald-600 text-white py-2 px-4 border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 focus:outline-none w-full"}>업로드</button>
         </div>
     );
 };
