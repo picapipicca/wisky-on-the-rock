568d9f0fc00fa8f0fa9c9f72e719e68613fb884f
picapipicca <haewon09106@gmail.com>
2022-11-24 01:10:59 +0900

    new:error message

--- components/atom/input.tsx
+++ components/atom/input.tsx
@@ -1,51 +1,77 @@
-import {cls} from "@libraries/client/utils";
+import { cls } from "@libraries/client/utils";
 import type { UseFormRegisterReturn } from "react-hook-form";
 
 interface InputProps {
-    leftInnerLabel?: string;
-    rightInnerLabel?: string;
-    placeholder?: string;
-    outerLabel?: string;
-    isRequired?: boolean;
-    name?: string;
-    type?: "email" | "number" | "text";
-    register:UseFormRegisterReturn;
+  leftInnerLabel?: string;
+  rightInnerLabel?: string;
+  placeholder?: string;
+  outerLabel?: string;
+  isRequired?: boolean;
+  type?: "email" | "number" | "text";
+  register: UseFormRegisterReturn;
+  error?: any;
 }
 
 //TODO : OuterLabel 위치 손보기 input components 에 고정? 밖으로 뺄것인지?
-const Input = ({   type="text",
-                   name,
-                   isRequired = false,
-                   leftInnerLabel,
-                   rightInnerLabel,
-                   placeholder,
-                   outerLabel,
-                   register,
-               }: InputProps) => {
-    return (
-        <div>
-            {outerLabel && <label htmlFor={name} className="mb-1.5 block text-sm font-medium text-gray-700">
-                {outerLabel}
-            </label>}
-            <div className="rounded-md relative flex shadow-sm">
-                {leftInnerLabel &&
-                    <span
-                        className={"flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm"}>{leftInnerLabel}</span>}
-                <input id={name}
-                       {...register}
-                       placeholder={placeholder}
-                       type={type}
-                       className={cls(leftInnerLabel ? "px-8 rounded-l-none" : rightInnerLabel ? "pr-20" : "", "px-3 appearance-none w-full py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500")}
-                       required={isRequired}
-                />
-                {rightInnerLabel &&
-                    <div
-                        className={"py-2.5 absolute pointer-events-none right-0 px-3 border-l flex items-center justify-center"}>
-                        <span className={"text-gray-500 text-sm"}>{rightInnerLabel}</span>
-                    </div>}
-            </div>
-        </div>
-    );
+const Input = ({
+  type = "text",
+  isRequired = false,
+  leftInnerLabel,
+  rightInnerLabel,
+  placeholder,
+  outerLabel,
+  register,
+  error,
+}: InputProps) => {
+   
+  return (
+    <div>
+      {outerLabel && (
+        <label
+          htmlFor={register.name}
+          className="mb-1.5 block text-sm font-medium text-gray-700"
+        >
+          {outerLabel}
+        </label>
+      )}
+      <div className="rounded-md relative flex shadow-sm">
+        {leftInnerLabel && (
+          <span
+            className={
+              "flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm"
+            }
+          >
+            {leftInnerLabel}
+          </span>
+        )}
+        <input
+          id={register.name}
+          {...register}
+          placeholder={placeholder}
+          type={type}
+          className={cls(
+            leftInnerLabel
+              ? "px-8 rounded-l-none"
+              : rightInnerLabel
+              ? "pr-20"
+              : "",
+            "px-3 appearance-none w-full py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500"
+          )}
+          required={isRequired}
+        />
+        {rightInnerLabel && (
+          <div
+            className={
+              "py-2.5 absolute pointer-events-none right-0 px-3 border-l flex items-center justify-center"
+            }
+          >
+            <span className={"text-gray-500 text-sm"}>{rightInnerLabel}</span>
+          </div>
+        )}
+      </div>
+      <span className="text-red-500 font-semibold text-sm">{error[register.name]?.message}</span>
+    </div>
+  );
 };
 
 export default Input;
--- components/atom/textarea.tsx
+++ components/atom/textarea.tsx
@@ -1,23 +1,45 @@
-import type {NextPage} from "next";
+import type { NextPage } from "next";
+import type { UseFormRegisterReturn } from "react-hook-form";
 
 interface TextAreaProps {
-    label?: string;
-    placeholder?: string;
-    rows?: number;
-    id?:string;
+  label?: string;
+  placeholder?: string;
+  rows?: number;
+  register: UseFormRegisterReturn;
+  isRequired?: boolean;
+  error:any;
 }
 
-const Textarea = ({id,label, placeholder = "", rows = 4}: TextAreaProps) => {
-    return (
-        <div>
-            {label && <label htmlFor={id} className="text-sm font-medium text-gray-700 mb-1 block">{label}</label>}
-            <textarea
-                id={id}
-                className="mt-1 shadow-sm w-full focus:ring-emerald-500 rounded-md border-gray-300 focus:border-emerald-500 "
-                rows={rows}
-                placeholder={placeholder}/>
-        </div>
-    );
+const Textarea = ({
+  isRequired = false,
+  register,
+  label,
+  placeholder = "",
+  rows = 4,
+  error
+}: TextAreaProps) => {
+  console.log(error)
+  return (
+    <div>
+      {label && (
+        <label
+          htmlFor={register.name}
+          className="text-sm font-medium text-gray-700 mb-1 block"
+        >
+          {label}
+        </label>
+      )}
+      <textarea
+        {...register}
+        id={register.name}
+        className="mt-1 shadow-sm w-full focus:ring-emerald-500 rounded-md border-gray-300 focus:border-emerald-500 "
+        rows={rows}
+        placeholder={placeholder}
+        required={isRequired}
+      />
+      <span className="text-red-500 font-semibold text-sm">{error[register.name]?.message}</span>
+    </div>
+  );
 };
 
 export default Textarea;
