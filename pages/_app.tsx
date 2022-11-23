6fb89530cf044c7381ee547812ac62c05aa8136b
picapipicca <haewon09106@gmail.com>
2022-11-24 01:28:37 +0900

    new: swr config fetcher globalization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import '../styles/globals.css'
-import type {AppProps} from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
 
-export default function App({Component, pageProps}: AppProps) {
-    return (
-        // max-w-lg: 512px / xs: 320px / sm: 384px / xl: 576px
-        <div className={"w-full max-w-xl mx-auto"} >
-            <Component {...pageProps} />
-        </div>
-    )
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+      // max-w-lg: 512px / xs: 320px / sm: 384px / xl: 576px
+    <SWRConfig value={{fetcher:(url: string) => fetch(url).then((res) => res.json())}}>
+      <div className={"w-full max-w-xl mx-auto"}>
+        <Component {...pageProps} />
+      </div>
+    </SWRConfig>
+  );
 }
