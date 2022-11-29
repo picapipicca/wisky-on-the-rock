08993d8ea7d877b34556c3398c626502ea695e5f
picapipicca <haewon09106@gmail.com>
2022-11-30 02:12:29 +0900

    new: sql query 확인하는 주석 추가

--- libraries/server/client.ts
+++ libraries/server/client.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 declare global {
   var client: PrismaClient | undefined;
 }
+// const client = global.client || new PrismaClient({ log: ["query"] });
 const client = global.client || new PrismaClient();
 
 if (process.env.NODE_ENV === "development") {
