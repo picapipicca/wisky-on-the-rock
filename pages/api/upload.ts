27ef41e89e8bf66aea6c5539acdb72ceac421a6d
picapipicca <haewon09106@gmail.com>
2023-01-06 17:39:20 +0900

    new:post ,get cloudflare

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -7,14 +7,14 @@ const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) => {
-const response = await (await fetch(`https://api.cloudflare.com/client/v4/accounts/${process.env.CF_IMAGE_ID}/images/v2/direct_upload`,{
+const response = await (await fetch(`https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ID}/images/v2/direct_upload`,{
   method:"POST",
   headers:{
     "Content-Type" :"application/json",
     "Authorization": `Bearer ${process.env.CF_IMAGE_TOKEN}`,
   },
 })).json();
-console.log(response);
+
   res.json({
     ok: true,
     ...response.result
